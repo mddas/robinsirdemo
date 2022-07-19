f802ac777dff743ff99c3ac5c07f8ad546de0027
md <mddasgudiya@gmail.com>
2022-07-19 23:16:36 +0545

    .

--- app/Http/Middleware/Authenticate.php
+++ app/Http/Middleware/Authenticate.php
@@ -15,7 +15,7 @@ class Authenticate extends Middleware
     protected function redirectTo($request)
     {
         if (! $request->expectsJson()) {
-            return route('userlogin');
+            return route('login');
         }
     }
 }
